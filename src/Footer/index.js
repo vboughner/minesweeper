a49cc944a65126fba5043a7130e37c6b5b341d61
van <van@hovercloud.com>
2018-04-16 09:19:46 -0700

    fixed typo in the footer for github link

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -7,7 +7,7 @@ class Footer extends Component {
       <div className="footer">
         <p>
           Written with React by <a href="https://www.linkedin.com/in/vboughner">Van Boughner</a> (
-          <a href="https://github.com/vboughner/mineswepper">source on github</a>)
+          <a href="https://github.com/vboughner/minesweeper">source on github</a>)
         </p>
       </div>
     );
